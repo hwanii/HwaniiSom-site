043afa4d828f211d062f890e9e1595968ad7be04
hwanii <yth3260@gmail.com>
2017-12-30 02:11:03 +0900

    Changed settings.py for pythonanywhere.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u#319ry55m0!k0v@y6%av8wl&to0$%&j!n-np!2rar-zugc&q%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
